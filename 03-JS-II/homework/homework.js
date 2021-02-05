f3797ae88809e2c9319d53410e12fe53836a044a
rglvlncsr <rglvlncsr@gmail.com>
2021-02-05 16:39:29 -0600

    i confuse = con ===

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -180,8 +180,12 @@ function esPrimo(numero) {
 function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
   //si su valor es true y “Soy falso” si su valor es false.
-  //Escribe tu código aquí
+    //Escribe tu código aquí
 
+    if(valor === true) {
+	return "Soy verdadero"
+    }
+    return("Soy falso")
 }
 
 function tablaDelSeis(){
